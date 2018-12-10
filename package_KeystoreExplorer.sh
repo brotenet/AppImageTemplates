7177fc69b25fc1bc101e8923df0ee25189a1de8c
Brotenet <brotenet@gmail.com>
2018-12-10 09:43:08 +0000

    Update package_KeystoreExplorer.sh

--- package_KeystoreExplorer.sh
+++ package_KeystoreExplorer.sh
@@ -6,9 +6,9 @@ BINTRAY_USER=$1
 BINTRAY_REPO=$2
 BINTRAY_API_KEY=$3
 
-DOWNLOAD_URL=https://github.com/kaikramer/keystore-explorer/releases/download/v5.4.0/kse-540.zip
+DOWNLOAD_URL=https://github.com/kaikramer/keystore-explorer/releases/download/v5.4.1/kse-541.zip
 NAME=KeystoreExplorer
-VERSION=5.4.0
+VERSION=5.4.1
 ARCH=x86_64
 
 wget $DOWNLOAD_URL -O download.zip
